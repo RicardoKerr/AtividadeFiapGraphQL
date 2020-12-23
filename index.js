e2f1979674e9987f37016e2491ed70166c8b0c3e
RicardoKerr <ricardo.kerr@gmail.com>
2020-12-22 23:06:51 -0300

    teste

---
+++ index.js
@@ -0,0 +1,43 @@
+const {ApolloServer, gql} = require('apollo-server')
+
+const typeDefs = gql`
+    type Produto{
+        id:Int
+        nomeproduto:String
+        descricao:String
+        fornecedor:String
+        preco:Float
+        datacadastro:String       
+    }
+    type Query{
+        produto:Produto
+
+
+    }
+    type Mutation{
+
+
+
+    }
+
+`
+const resolvers = {
+    Query:{
+        produto(){
+            return{
+                id:45,
+                nomeproduto:"mouse",
+                descricao:"mouse sem fio",
+                fornecedor:"DELL",
+                preco:100.00,
+                datacadastro:"15/12/2020"
+            }
+        }
+
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+server.listen().then(({url})=>console.log(`Servidor online ... ${url}`))
