e78a3c81fadb43018e69d90c982d3a43b0edd060
RicardoKerr <ricardo.kerr@gmail.com>
2020-12-23 15:05:50 -0300

    functionindexvalidado

--- functions/index.js
+++ functions/index.js
@@ -10,71 +10,13 @@ admin.initializeApp({
     databaseURL: "https://fiapkerrbanco-default-rtdb.firebaseio.com/"
   });
 
-const typeDefs = gql`
-    type produto{
-        id:Int
-        nomeproduto:String
-        descricao:String
-        fornecedor:String
-        preco:Float
-        datacadastro:String
-    }
-    type Query{
-        produto:[produto]
-    }
-    type Mutation{
-        novoproduto(
-            id:Int
-            nomeproduto:String
-            descricao:String
-            fornecedor:String
-            preco:Float
-            datacadastro:String
-        ):[produto]
-    }
-`
-const resolvers = {
-    Query:{
-        produto(){
-            return admin
-                    .database()
-                    .ref("produtos")
-                    .once("value")
-                    .then(snap => snap.val())
-                    .then(val => Object.keys(val)
-                    .map((key)=>val[key]))
-             }
-         },
-        
-         Mutation:{
-            novoproduto(_,{id,nomeproduto,descricao,fornecedor,preco,datacadastro}){
-                const novo = {
-                id:id,
-                nomeproduto:nomeproduto,
-                descricao:descricao,
-                fornecedor:fornecedor,
-                preco:preco,
-                datacadastro:datacadastro
-            }
-            return admin
-                .database()
-                .ref("produtos")
-                .push(novo)
-                .then(snap => snap.val())
-                .then(val => Object.keys(val)
-                .map((key)=>val[key]))
-            }
-        }
-}
+  const app = express();
 
-const app = express()
-
-const server = new ApolloServer({
-    typeDefs,resolvers
-})
-
-server.applyMiddleware({app,path:"/",cors:true})
-
-exports.graphql = functions.https.onRequest(app)
-
-
+  const server = new ApolloServer({
+    typeDefs: importSchema("./schema/index.graphql"),
+    resolvers: resolvers,
+  });
+  
+  server.applyMiddleware({ app, path: "/", cors: true });
+  
+  exports.graphql = functions.https.onRequest(app);
