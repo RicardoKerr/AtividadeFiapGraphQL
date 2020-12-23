4fd64a3ce1844febef50755d194e1f4868986acf
RicardoKerr <ricardo.kerr@gmail.com>
2020-12-23 00:09:43 -0300

    ok

--- index.js
+++ index.js
@@ -1,44 +1,7 @@
 const {ApolloServer, gql} = require('apollo-server')
 
-const typeDefs = gql`
-    type Produto{
-        id:Int
-        nomeproduto:String
-        descricao:String
-        fornecedor:String
-        preco:Float
-        datacadastro:String       
-    }
-
-    type Cliente{
-        id:Int
-        nomecliente:String
-    }
-
-    type Query{
-        produto:Produto
-        cliente:Cliente
-    }
-`
 const resolvers = {
-    Query:{
-        produto(){
-            return{
-                id:45,
-                nomeproduto:"mouse",
-                descricao:"mouse sem fio",
-                fornecedor:"DELL",
-                preco:100.00,
-                datacadastro:"15/12/2020"
-            }
-        },
-        cliente(){
-            return{
-                id:12,
-                nomecliente:"Marcos"
-            }
-        }
-    }
+    
 }
 
 const server = new ApolloServer({
